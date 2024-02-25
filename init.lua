df0b483a36e5c169dcc530f57bbaac00350ecb0e
goropikari <goropikari56@gmail.com>
2024-02-25 14:05:58 +0900

    refactor

--- init.lua
+++ init.lua
@@ -77,9 +77,6 @@ require('lazy').setup(
     -- Detect tabstop and shiftwidth automatically
     'tpope/vim-sleuth',
 
-    -- NOTE: This is where your plugins related to LSP can be installed.
-    --  The configuration is done below. Search for lspconfig to find it below.
-
     {
       -- Autocompletion
       'hrsh7th/nvim-cmp',
@@ -113,90 +110,12 @@ require('lazy').setup(
     {
       -- Adds git related signs to the gutter, as well as utilities for managing changes
       'lewis6991/gitsigns.nvim',
-      opts = {
-        -- See `:help gitsigns.txt`
-        signs = {
-          add = { text = '+' },
-          change = { text = '~' },
-          delete = { text = '_' },
-          topdelete = { text = '‾' },
-          changedelete = { text = '~' },
-        },
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
-
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
-
-          -- Navigation
-          map({ 'n', 'v' }, ']c', function()
-            if vim.wo.diff then
-              return ']c'
-            end
-            vim.schedule(function()
-              gs.next_hunk()
-            end)
-            return '<Ignore>'
-          end, { expr = true, desc = 'Jump to next hunk' })
-
-          map({ 'n', 'v' }, '[c', function()
-            if vim.wo.diff then
-              return '[c'
-            end
-            vim.schedule(function()
-              gs.prev_hunk()
-            end)
-            return '<Ignore>'
-          end, { expr = true, desc = 'Jump to previous hunk' })
-
-          -- Actions
-          -- visual mode
-          map('v', '<leader>hs', function()
-            gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-          end, { desc = 'stage git hunk' })
-          map('v', '<leader>hr', function()
-            gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-          end, { desc = 'reset git hunk' })
-          -- normal mode
-          map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
-          map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
-          map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-          map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-          map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-          map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-          map('n', '<leader>hb', function()
-            gs.blame_line { full = false }
-          end, { desc = 'git blame line' })
-          map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-          map('n', '<leader>hD', function()
-            gs.diffthis '~'
-          end, { desc = 'git diff against last commit' })
-
-          -- Toggles
-          -- map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-          -- map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
-
-          -- Text object
-          map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
-        end,
-      },
     },
 
     {
       -- Set lualine as statusline
       'nvim-lualine/lualine.nvim',
       -- See `:help lualine.txt`
-      opts = {
-        options = {
-          icons_enabled = false,
-          theme = 'auto',
-          component_separators = '|',
-          section_separators = '',
-        },
-      },
     },
 
     {
@@ -268,49 +187,12 @@ require('lazy').setup(
           end
         end
       end,
-      opts = {
-        sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-        open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
-        filesystem = {
-          bind_to_cwd = false,
-          follow_current_file = { enabled = true },
-          use_libuv_file_watcher = true,
-        },
-        window = {
-          mappings = {
-            ["<space>"] = "none",
-            ["Y"] = function(state)
-              local node = state.tree:get_node()
-              local path = node:get_id()
-              vim.fn.setreg("+", path, "c")
-            end,
-          },
-        },
-        default_component_configs = {
-          indent = {
-            with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-            expander_collapsed = "",
-            expander_expanded = "",
-            expander_highlight = "NeoTreeExpander",
-          },
-        },
-      },
     },
 
     {
       -- vimium のような感じでコードジャンプできる
       "folke/flash.nvim",
       event = "VeryLazy",
-      ---@type Flash.Config
-      opts = {},
-      -- stylua: ignore
-      keys = {
-        { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-        { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-        { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-        { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-      },
     },
 
     {
@@ -327,10 +209,9 @@ require('lazy').setup(
     --       These are some example plugins that I've included in the kickstart repository.
     --       Uncomment any of the lines below to enable them.
     -- require 'kickstart.plugins.autoformat',
-    require 'plugins.language',
-    require 'plugins.debug',
     require 'plugins.editor',
-    require 'plugins.test_suite',
+    require 'plugins.language',
+    require 'plugins.test',
 
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
     --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
@@ -397,6 +278,9 @@ require 'custom.plugins.treesitter'
 
 -- [[ Configure LSP ]]
 require 'custom.plugins.lsp'
+
+-- [[ Configure Test ]]
+require 'custom.plugins.test'
 
 -- [[ Configure nvim-cmp ]]
 require 'custom.plugins.nvim_cmp'
---
+++ lua/plugins/test.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    -- NOTE: Yes, you can install new plugins here!
+    'mfussenegger/nvim-dap',
+    -- NOTE: And you can specify dependencies as well
+    dependencies = {
+      -- Creates a beautiful debugger UI
+      'rcarriga/nvim-dap-ui',
+
+      -- Add your own debuggers here
+      'leoluz/nvim-dap-go',
+    },
+  },
+
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+
+      -- language adapter
+      -- "nvim-neotest/neotest-go",
+      {
+        url = "https://github.com/wwnbb/neotest-go",
+        branch = "feat/dap-support",
+      },
+    },
+  },
+}
