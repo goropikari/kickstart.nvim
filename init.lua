921d29f3dc8ad7e320a6c4cc3602768404427453
goropikari <goropikari56@gmail.com>
2024-04-23 00:16:53 +0900

    buffer command

--- init.lua
+++ init.lua
@@ -108,6 +108,7 @@ require('lazy').setup(
             },
           },
           window = {
+            position = 'float',
             mappings = {
               ["<space>"] = "none",
               ["Y"] = function(state)
@@ -170,10 +171,7 @@ require('lazy').setup(
       },
       init = function() vim.g.barbar_auto_setup = false end,
       opts = {
-        -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-        -- animation = true,
-        -- insert_at_start = true,
-        -- …etc.
+        animation = false,
       },
       version = '^1.0.0', -- optional: only update when a new 1.x version is released
     },
