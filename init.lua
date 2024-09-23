375e0242c26a9dccd5e9e35cb5ce0e51bbd8c5b6
goropikari <goropikari56@gmail.com>
2024-09-23 21:58:12 +0900

    refac lualine

--- init.lua
+++ init.lua
@@ -56,6 +56,22 @@ require('lazy').setup(
     {
       -- Set lualine as statusline
       'nvim-lualine/lualine.nvim',
+      opts = {
+        options = {
+          icons_enabled = false,
+          theme = 'auto',
+          component_separators = '|',
+          section_separators = '',
+        },
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 3,
+            },
+          },
+        },
+      },
     },
     {
       -- indent を見やすくする
