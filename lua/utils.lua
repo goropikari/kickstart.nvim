1ddf57b62817a1e1da31c21333b15b2e0900edc3
goropikari <goropikari56@gmail.com>
2024-10-03 00:25:40 +0900

    add line selection fn

--- lua/utils.lua
+++ lua/utils.lua
@@ -34,4 +34,27 @@ function M.live_grep_git_root()
   end
 end
 
+-- 選択した行を取得する
+---@return table<string>
+function M.get_visual_lines(opts)
+  if vim.fn.mode() == 'n' then -- command から使う用
+    return vim.fn.getline(opts.line1, opts.line2)
+  else -- <leader> key を使った keymap 用
+    local lines = vim.fn.getregion(vim.fn.getpos('v'), vim.fn.getpos('.'), { type = vim.fn.mode() })
+    -- https://github.com/neovim/neovim/discussions/26092
+    vim.cmd([[ execute "normal! \<ESC>" ]])
+    return lines
+  end
+end
+
+function M.show_visual_lines(opts)
+  print(vim.fn.join(M.get_visual_lines(opts) or {}, '\n'))
+end
+
+-- vim.api.nvim_create_user_command('ShowVisualLines', function(opts)
+--   show_visual_lines(opts)
+-- end, {
+--   range = 0, -- これを入れないと No range allowed というエラーが出る
+-- })
+
 return M
