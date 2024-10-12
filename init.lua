ae875dc37732a866991db6b648bc19c9eaecad42
goropikari <goropikari56@gmail.com>
2024-10-12 21:27:19 +0900

    delete custom plugin

--- init.lua
+++ init.lua
@@ -4,15 +4,18 @@ require('base')
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -1093,10 +1096,11 @@ require('lazy').setup({
       },
     },
 
-    { import = 'plugins.filetypes' },
-    { import = 'plugins.lsp' },
-    { import = 'plugins.test' },
-    { import = 'plugins.llm' },
+    -- { import = 'plugins.filetypes' },
+    -- { import = 'plugins.lsp' },
+    -- { import = 'plugins.test' },
+    -- { import = 'plugins.llm' },
+    { import = 'plugins' },
     require('custom.plugins'),
   },
 
@@ -1115,19 +1119,24 @@ require('lazy').setup({
     notify = false, -- get a notification when changes are found
   },
   performance = {
+    cache = {
+      enabled = true,
+    },
+    reset_packpath = true, -- reset the package path to improve startup time
     rtp = {
       disabled_plugins = {
-        'netrw',
-        'netrwPlugin',
-        'netrwSettings',
+        'commenting',
         'gzip',
-        -- "matchit",
-        -- "matchparen",
-        'netrwPlugin',
         'tarPlugin',
         'tohtml',
         'tutor',
         'zipPlugin', -- "netrwFileHandlers",
+        -- "matchit",
+        -- "matchparen",
+        -- 'netrw',
+        -- 'netrwPlugin',
+        -- 'netrwPlugin',
+        -- 'netrwSettings',
       },
     },
   },
--- lua/custom/plugins/prompt-chat.nvim/prompt-chat.lua
+++
@@ -1,4 +0,0 @@
-if vim.g.prompt_chat == 1 then
-  return
-end
-vim.g.prompt_chat = 1
